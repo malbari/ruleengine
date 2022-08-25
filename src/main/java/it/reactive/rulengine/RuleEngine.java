28d8bc809216dd7553049b47586159354a13272a
Maurizio Albari <maurizio.albarigmail.com>
2022-08-25 13:42:32 +0200

    fixed

--- src/main/java/it/reactive/rulengine/RuleEngine.java
+++ src/main/java/it/reactive/rulengine/RuleEngine.java
@@ -14,59 +14,47 @@ import org.graalvm.polyglot.proxy.ProxyObject;
 
 @SuppressWarnings("restriction")
 public class RuleEngine {
-	static Logger logger = Logger.getLogger(RuleEngine.class.getName());
+	private static Logger logger = Logger.getLogger(RuleEngine.class.getName());
 
-	static String[] SAMPLE_RULES = { "rule01", "rule02" };
+	private static String[] SAMPLE_RULES = { "rule01", "rule02" };
 
-	private String[] rules;
+	private ThreadLocal<Context> ruleContext = new ThreadLocal<Context>();
 
-	private Context ruleContext;
-	
 	private boolean logToSysout = false;
 
 	private Map<String, Value> ruleEvalMap = new HashMap<String, Value>();
-
-	public void setRules(String[] rules) {
-		this.rules = rules;
-	}
 
 	public static void main(String[] args) throws Exception {
 		RuleEngine re = new RuleEngine();
 		re.doSample();
 	}
 
-	public RuleEngine() {
-		ruleContext = Context.create();
-	}
-	
 	private void log(String text) {
 		if (logToSysout) {
 			System.out.println(text);
 		} else {
 			logger.info(text);
 		}
-		
+
 	}
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	private void doSample() throws IOException {
 		logToSysout = true;
-		
-		setRules(SAMPLE_RULES);
 
 		Map inputMap = new HashMap();
 		inputMap.put("uno", "1");
 		inputMap.put("due", "2");
 		log("JAVA-input: " + inputMap);
 
-		Map outputMap = evaluateRules(inputMap);
+		Map outputMap = evaluateRules(SAMPLE_RULES, inputMap);
 
 		log("*** termine esecuzione regole *** ");
 		log("JAVA-output: " + outputMap);
 	}
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public Map evaluateRules(Map contextMap) throws IOException {
+	public Map evaluateRules(String[] rules, Map contextMap) throws IOException {
 		for (String rule : rules) {
 			log("*** REGOLA " + rule + " ***");
 
@@ -88,9 +76,18 @@ public class RuleEngine {
 		if (ruleEval == null) {
 			Path rulePath = Paths.get("./src/test/resources/" + rule + ".js");
 			String ruleText = new String(Files.readAllBytes(rulePath));
-			ruleEval = ruleContext.eval("js", "(" + ruleText + ")");
+			ruleEval = getRuleContext().eval("js", "(" + ruleText + ")");
 			ruleEvalMap.put(rule, ruleEval);
 		}
 		return ruleEval;
 	}
+
+	private Context getRuleContext() {
+		Context c = (Context) ruleContext.get();
+		if (c == null) {
+			c = Context.create();
+			ruleContext.set(c);
+		}
+		return c;
+	};
 }
